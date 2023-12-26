3b0563f2099b6c98c050c1a9cabdeb1673199e42
Nicoly-Almeida <nicolyejady@gmail.com>
2023-12-26 09:50:56 -0300

    first commit

--- src/main/java/Consumidor.java
+++ src/main/java/Consumidor.java
@@ -8,22 +8,40 @@ public class Consumidor {
     public static void main(String[] args) throws Exception {
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+
         Connection conexao = connectionFactory.newConnection();
         Channel canal = conexao.createChannel();
 
-        String NOME_FILA = "plica"
-                + "";
-        canal.queueDeclare(NOME_FILA, false, false, false, null);
+        String NOME_FILA = "teste";
+        canal.queueDeclare(NOME_FILA, true, false, false, null);
+        canal.basicQos(1);
 
         DeliverCallback callback = (consumerTag, delivery) -> {
             String mensagem = new String(delivery.getBody());
-            System.out.println("Eu " + consumerTag + " Recebi: " + mensagem);
+            System.out.println("Eu, " + consumerTag + ", recebi: " + mensagem);
+
+            try {
+                doWork(mensagem);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                System.out.println(" [x] Done");
+                canal.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
         };
 
-        // fila, noAck, callback, callback em caso de cancelamento (por exemplo, a fila foi deletada)
-        canal.basicConsume(NOME_FILA, true, callback, consumerTag -> {
-            System.out.println("Cancelaram a fila: " + NOME_FILA);
+        canal.basicConsume(NOME_FILA, false, callback, consumerTag -> {
+            System.out.println("Fila cancelada: " + NOME_FILA);
         });
+    }
+
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch: task.toCharArray()) {
+            if (ch == '.') Thread.sleep(1000);
+        }
     }
 }
 
